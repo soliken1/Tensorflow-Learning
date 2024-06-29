4ed9cd0e223c172b458fd2ba0cc5ba5e0fa5cac8
Soliken <kennethrex456@gmail.com>
2024-06-29 15:20:46 +0800

    Regression Update

--- Regression/Regression.py
+++ Regression/Regression.py
@@ -4,6 +4,9 @@ import numpy as np
 import sklearn
 from sklearn import linear_model
 from sklearn.utils import shuffle
+import matplotlib.pyplot as pyplot
+import pickle
+from matplotlib import style
 
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 
@@ -15,14 +18,26 @@ predict = "G3"
 
 X = np.array(data.drop([predict], axis=1))
 y = np.array(data[predict])
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1)
 
-x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size = 0.1)
+"""
+best = 0
+for _ in range(30):
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1)
 
-linear = linear_model.LinearRegression()
+    linear = linear_model.LinearRegression()
 
-linear.fit(x_train, y_train)
-acc = linear.score(x_test, y_test)
-print(acc)
+    linear.fit(x_train, y_train)
+    acc = linear.score(x_test, y_test)
+    print(acc)
+
+    if acc > best:
+        best = acc
+        with open("studentmodel.pickle", "wb") as f:
+            pickle.dump(linear, f)
+"""
+pickle_in = open("studentmodel.pickle", "rb")
+linear = pickle.load(pickle_in)
 
 print('Co: ',  linear.coef_)
 print('Intercept: ', linear.intercept_)
@@ -31,3 +46,10 @@ predictions = linear.predict(x_test)
 
 for x in range(len(predictions)):
     print(predictions[x], x_test[x], y_test[x])
+
+p = "absences"
+style.use("ggplot")
+pyplot.scatter(data[p], data["G3"])
+pyplot.xlabel(p)
+pyplot.ylabel("Final Grade")
+pyplot.show()
